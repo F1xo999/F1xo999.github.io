6dd36fcf2f40f904ddfdb827ec6c9e08acd8054b
F1xo999 <134588494+F1xo999@users.noreply.github.com>
2023-05-25 11:08:25 +0200

    Update button.js

--- button.js
+++ button.js
@@ -1,11 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("myButton");
-    button.addEventListener("click", function() {
-      // Handle button click event
-      // Add your desired functionality here
-      alert("Button clicked!");
-      // You can redirect the user to a specific page using window.location.href
-      // Example: window.location.href = "https://example.com";
-    });
-  });
+
   
