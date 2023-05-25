64ff51a7c0bd84410674835d755e507d92b5ed6d
F1xo999 <spyfilip@gmail.com>
2023-05-25 11:02:40 +0200

    ez

---
+++ button.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+      // Handle button click event
+      // Add your desired functionality here
+      alert("Button clicked!");
+      // You can redirect the user to a specific page using window.location.href
+      // Example: window.location.href = "https://example.com";
+    });
+  });
+  
