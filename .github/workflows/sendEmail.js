a39ce7a39b6646b408ae4701b6e39cd92f215fc7
F1xo999 <134588494+F1xo999@users.noreply.github.com>
2023-05-30 08:59:17 +0200

    Update sendEmail.js

--- .github/workflows/sendEmail.js
+++ .github/workflows/sendEmail.js
@@ -1,1 +1,22 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const msg = {
+  to: 'spyfilip@gmail.com', // Replace with your recipient's email address
+  from: 'argentinos1050@gmail.com', // Replace with your verified sender's email address
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent');
+    process.exit(0); // Exit with a zero code to indicate success
+  })
+  .catch((error) => {
+    console.error('Error sending email:', error);
+    process.exit(1); // Exit with a non-zero code to indicate failure
+  });
 
