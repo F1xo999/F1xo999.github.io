4ca02985524fe388b99b8af3c1f449d474f50c02
F1xo999 <134588494+F1xo999@users.noreply.github.com>
2023-05-30 09:38:21 +0200

    Update sendEmail.js

--- .github/workflows/sendEmail.js
+++ .github/workflows/sendEmail.js
@@ -1,1 +1,29 @@
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  port: 465, // Change the port as needed (25, 587, or 465)
+  secure: false, // false for unencrypted/TLS connections, true for SSL connections
+  auth: {
+    user: 'apikey',
+    pass: process.env.SENDGRID_API_KEY
+  }
+});
+
+const mailOptions = {
+  from: 'test@example.com',
+  to: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error sending email:', error);
+    process.exit(1);
+  } else {
+    console.log('Email sent:', info.messageId);
+    process.exit(0);
+  }
+});
